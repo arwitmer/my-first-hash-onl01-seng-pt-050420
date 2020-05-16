89fece894c84b82568a5fc4ae5b95868433efd54
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 09:20:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   
-  my_hash = {"hash brown hash"}
+  my_hash = hash_brown_hash
 
   
 end
